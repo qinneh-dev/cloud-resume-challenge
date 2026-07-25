8891d2369e1dc65e87ee36f1e65c3305c5c61936
Omar Alqinneh <omaralqinneh@Omars-MacBook-Pro.local>
2026-07-25 05:14:48 +0300

    feat: Configure remote S3 backend for Terraform state sharing

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,32 +1,39 @@
-terraform{
-    required_version = ">= 1.5.0"
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
+  backend "s3" {
+    bucket         = "omaralqinneh-resume-bucket-eu"
+    key            = "terraform/state.tfstate"
+    region         = "eu-central-1"
+  }
 }
+
 provider "aws" {
-    region = "eu-central-1"
-    default_tags {
-        tags = {
-            Project = "cloud-resume-challenge"
-            ManagedBy = "Terraform"
-            Environment = "Production"
-            Owner = "OmarAlqinneh"
-        }
+  region = "eu-central-1"
+  default_tags {
+    tags = {
+      Project     = "cloud-resume-challenge"
+      ManagedBy   = "Terraform"
+      Environment = "Production"
+      Owner       = "OmarAlqinneh"
     }
+  }
 }
+
 provider "aws" {
-    alias  = "useast1"
-    region = "us-east-1"
-    default_tags {
-        tags = {
-            Project = "cloud-resume-challenge"
-            ManagedBy = "Terraform"
-            Environment = "Production"
-            Owner = "OmarAlqinneh"
-        }
+  alias  = "useast1"
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      Project     = "cloud-resume-challenge"
+      ManagedBy   = "Terraform"
+      Environment = "Production"
+      Owner       = "OmarAlqinneh"
     }
+  }
 }
